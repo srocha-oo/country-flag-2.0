36e192a862943d46dd02093823fb369939558535
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-02-20 12:20:04 -0600

    Initial Commit

---
+++ country flag 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  country flag 2.0
+//
+//  Created by stephanie rocha marquez on 2/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ country flag 2.0/country_flag_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  country_flag_2_0App.swift
+//  country flag 2.0
+//
+//  Created by stephanie rocha marquez on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct country_flag_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
