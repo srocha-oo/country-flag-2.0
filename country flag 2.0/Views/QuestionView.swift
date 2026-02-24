d6a53a319cc9437818f4e40a273b220ad7a94274
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-02-24 12:41:37 -0600

    Added QuestionView

---
+++ country flag 2.0/Views/QuestionView.swift
@@ -0,0 +1,47 @@
+//
+//  QuestionView.swift
+//  country flag 2.0
+//
+//  Created by stephanie rocha marquez on 2/24/26.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Text("Country Flag Game")
+                    .fontWeight(.heavy)
+                    .padding()
+                Spacer()
+                Text("1 out of 3")
+                    .padding()
+            }
+            ProgressBar(progress: 50)
+            VStack(spacing: 10) {
+                Text("Which country's flag is this?")
+                    .font(.title)
+                Image("Italy")
+                    .resizable()
+                    .frame(width: 300, height: 200)
+                AnswerRow(answer: Answer(text: "France"
+                                         , isCorrect: false))
+                AnswerRow(answer: Answer(text: "Germany"
+                                         , isCorrect: false))
+                AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
+                AnswerRow(answer: Answer(text: "England", isCorrect: false))
+            }
+                CustomButton(text: "Next")
+                Spacer ()
+            }
+            .foregroundStyle(.yellow)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.cyan)
+            .navigationBarBackButtonHidden(true)
+        }
+    }
+    #Preview {
+        QuestionView()
+    }
+    
