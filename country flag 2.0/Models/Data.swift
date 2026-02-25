087d98c49e8cb31e95e0cec3021703efd7e079ba
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-02-25 09:02:44 -0600

    Created Data and Game Manager

---
+++ country flag 2.0/Models/Data.swift
@@ -0,0 +1,32 @@
+//
+//  Data.swift
+//  country flag 2.0
+//
+//  Created by stephanie rocha marquez on 2/24/26.
+//
+
+import Foundation
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+}
